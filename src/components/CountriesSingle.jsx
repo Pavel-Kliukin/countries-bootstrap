f79e08cd2dd2ca08fcfae0223525be18a23cdfa0
Pavel-Kliukin <9213087@mail.ru>
2023-09-21 23:04:47 +0300

    Flag and name in the CountriesSingle.jsx styled

--- src/components/CountriesSingle.jsx
+++ src/components/CountriesSingle.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Card } from 'react-bootstrap';
 import { useLocation,useNavigate } from 'react-router-dom';
 import classes from './CSS/CountriesSingle.module.css'
 
@@ -33,7 +33,17 @@ const CountriesSingle = () => {
 
   return (
     <Container className={classes.countryContainer}>
-      <div>Single Country will be here</div>
+      <div className={classes.countryBox}>
+        <div className={classes.countryNameBox}>
+          <h2>{country.name.common}</h2>
+          <h4 className={classes.officialName}>{country.name.official}</h4>
+        </div>
+        <div className={classes.countryInfoBox}>
+          <div className={classes.flagBox}>
+            <Card.Img className={classes.flagIMG} src={country.flags.png} /> 
+          </div>
+        </div>
+      </div>
     </Container>
   );
 };
